3782e89c5dd23ec73c2e8ff4175f8ac582154682
jurozatto <juliarozatto6@gmail.com>
2025-10-21 19:50:53 -0300

    aula

---
+++ Academico/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Academico.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Aula()
+        {
+            return View();
+        }
+    }
+}
