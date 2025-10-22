016841e2faaeaa9e2a0cc550f9c3cce72e1d1b3f
jurozatto <juliarozatto6@gmail.com>
2025-10-21 21:16:54 -0300

    tabela

---
+++ Academico/Models/Aluno.cs
@@ -0,0 +1,12 @@
+﻿namespace Academico.Models
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; } //classe domínio, vira tabela no banco
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string CEP { get; set; }
+    }
+}
