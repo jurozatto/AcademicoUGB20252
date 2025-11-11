49f67a62fc170e2b47e6e57505d2575c24146f8e
jurozatto <juliarozatto6@gmail.com>
2025-11-11 20:15:20 -0300

    departamento

---
+++ Academico/Models/Departamento.cs
@@ -0,0 +1,11 @@
+﻿namespace Academico.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+     
+    }
+}
