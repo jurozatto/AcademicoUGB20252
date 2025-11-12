36b7f07cfac121e7261451fb5527fdd531f46ca1
jurozatto <juliarozatto6@gmail.com>
2025-11-11 21:20:03 -0300

    FK - Departamento/Instituição

--- Academico/Models/Departamento.cs
+++ Academico/Models/Departamento.cs
@@ -6,6 +6,8 @@
         public string Nome { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
-     
+        public int InstituicaoId { get; set; }
+        public Instituicao? InstituicaoOrigem { get; set; } //interrogação indica que o objeto pode ser nulo, e que o de cima será a chave estrangeira (não o InstituicaoOrigem)
+
     }
 }
